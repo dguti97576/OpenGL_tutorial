431eab8835eab0b3b9715e283715fad999607e48
dguti97576 <dgutierrez97576@gmail.com>
2022-08-10 12:09:23 -0700

    first push

---
+++ main.cpp
@@ -0,0 +1,44 @@
+//
+//  main.cpp
+//  OpenGL_Tutorial
+//
+//  Created by David Gutierrez on 8/10/22.
+//
+
+#include <GLFW/glfw3.h>
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(360, 360, "OpenGL_Window", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
